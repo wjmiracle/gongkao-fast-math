2ec45852bf783ff60830dbf13cdfceec9671d645
wjmiracle <162561647+wjmiracle@users.noreply.github.com>
2026-02-28 17:18:06 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/Home/HomeView.vue';
 import ModeSelect from '../views/Home/ModeSelect.vue';
 import GameEngine from '../views/Game/GameEngine.vue';
-import SettingsView from '../views/Settings/SettingsView.vue'; // 1. 引入设置页
+import SettingsView from '../views/Settings/SettingsView.vue'; 
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/select', name: 'Select', component: ModeSelect },
   { path: '/game/:mode', name: 'Game', component: GameEngine },
-  { path: '/settings', name: 'Settings', component: SettingsView } // 2. 添加路由规则
+  { path: '/settings', name: 'Settings', component: SettingsView } 
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  // 核心神级配置：使用 Hash 模式！彻底消灭静态服务器的 404 白屏问题！
+  history: createWebHashHistory(),
   routes
 });
 
