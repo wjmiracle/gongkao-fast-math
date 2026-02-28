0ddb6f43f1b4ccb6d9113107ac205e6f1178bc3f
wjmiracle <162561647+wjmiracle@users.noreply.github.com>
2026-02-28 17:16:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // 核心修复：告诉 Vite，我们的网站放在哪个子目录下！必须和你的 GitHub 仓库名一模一样！
-  base: '/gongkao-fast-math/', 
+  // 核心神级配置：改成相对路径！这样 Vercel 和 GitHub 都能完美识别！
+  base: './', 
 })
