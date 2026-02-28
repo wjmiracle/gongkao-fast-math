31f9eedfb8caf07f933396ae4ca61ac72a4421bb
wjmiracle <162561647+wjmiracle@users.noreply.github.com>
2026-02-28 17:11:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 核心修复：告诉 Vite，我们的网站放在哪个子目录下！必须和你的 GitHub 仓库名一模一样！
+  base: '/gongkao-fast-math/', 
 })
